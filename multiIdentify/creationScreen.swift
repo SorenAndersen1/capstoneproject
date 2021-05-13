64f2648fe1bf3b9c97db02641b53e073e0bc0292
SorenAndersen1 <andesore@oregonstate.edu>
2021-05-12 20:23:18 -0700

    Revert "Deleted useless files"
    This reverts commit 2a57b91a6a57df60123baf114bf1a12e15871e05.

---
+++ multiIdentify/creationScreen.swift
@@ -0,0 +1,20 @@
+//
+//  creationScreen.swift
+//  multiIdentify
+//
+//  Created by user183837 on 5/10/21.
+//
+
+import SwiftUI
+
+struct creationScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct creationScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        creationScreen()
+    }
+}
---
+++ multiIdentify/loginView.swift
@@ -0,0 +1,20 @@
+//
+//  loginScreen.swift
+//  multiIdentify
+//
+//  Created by user183837 on 5/10/21.
+//
+
+import SwiftUI
+
+struct loginView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct loginView_Previews: PreviewProvider {
+    static var previews: some View {
+        loginView()
+    }
+}
